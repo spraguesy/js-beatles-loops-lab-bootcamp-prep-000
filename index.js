afe1a1679f04590a2d758242b98ee060995421d7
Andrew Sprague <andrewsprague@outlook.com>
2018-07-13 15:39:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+
+function theBeatlesPlay (mus, inst) {
+  var newArr = [];
+  
+  for(var i = 0; i < mus.length; i++) {
+    var musInst = `${mus} plays ${inst}`;
+    newArr.push(musInst);
+  }
+  return newArr
+}
+
+function johnLennonFacts (facts) {
+  
+}
